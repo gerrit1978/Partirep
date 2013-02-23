40836b154b32a01fb30763473d873edf574cf53b
Gerrit Vos <gevo27@gmail.com>
2013-02-23 20:49:11 +0100

    Theming

--- sites/all/themes/hedgecomm/scripts/scripts.js
+++ sites/all/themes/hedgecomm/scripts/scripts.js
@@ -1,3 +1,12 @@
 jQuery(document).ready(function($) {
-  $("select, input, a.button, button").uniform();
+
+  // jquery uniform
+  $("select, input[type=submit]").uniform();
+  
+  // submit views exposed filters form (work packages)
+  $(".view-working-papers .views-exposed-form .views-submit-button").hide();
+  $(".view-working-papers .views-exposed-form select").change(function() {
+    $(".view-working-papers .view-content").css("opacity", "0.4");
+    $("#views-exposed-form-working-papers-page").css("opacity", "0.4").submit();
+  });
 });
